60c2975739b05b6f0f198a326ed0a13daf5bfb86
Mohammed Salah <145399512+MSALAHAH@users.noreply.github.com>
2023-09-30 18:27:51 -0400

    Update docScript.js

--- docScript.js
+++ docScript.js
@@ -131,7 +131,7 @@ function togglemainCheckbox8Options() {
     updateSentence();
 }
 
-                            // MAIN BOX 8 FUNCTION
+                            // SUB BOX 1-8 FUNCTION
                             function togglesubMainCheckbox1Options() {
                                 var subMainCheckbox1Checkbox = document.getElementById('subMainCheckbox1');
                                 var subMainCheckbox1Options = document.getElementById('subMainCheckbox1Options');
@@ -140,6 +140,58 @@ function togglemainCheckbox8Options() {
                                     subMainCheckbox1Options.style.display = 'block';
                                 } else {
                                     subMainCheckbox1Options.style.display = 'none';
+                                }
+                                updateSentence();
+                            }
+
+                            // SUB BOX 2-8 FUNCTION
+                            function togglesubMainCheckbox2Options() {
+                                var subMainCheckbox2Checkbox = document.getElementById('subMainCheckbox2');
+                                var subMainCheckbox2Options = document.getElementById('subMainCheckbox2Options');
+                            
+                                if (subMainCheckbox2Checkbox.checked) {
+                                    subMainCheckbox2Options.style.display = 'block';
+                                } else {
+                                    subMainCheckbox2Options.style.display = 'none';
+                                }
+                                updateSentence();
+                            }
+
+                            // SUB BOX 3-8 FUNCTION
+                            function togglesubMainCheckbox3Options() {
+                                var subMainCheckbox3Checkbox = document.getElementById('subMainCheckbox3');
+                                var subMainCheckbox3Options = document.getElementById('subMainCheckbox3Options');
+                            
+                                if (subMainCheckbox3Checkbox.checked) {
+                                    subMainCheckbox3Options.style.display = 'block';
+                                } else {
+                                    subMainCheckbox3Options.style.display = 'none';
+                                }
+                                updateSentence();
+                            }
+
+                            // SUB BOX 4-8 FUNCTION
+                            function togglesubMainCheckbox4Options() {
+                                var subMainCheckbox4Checkbox = document.getElementById('subMainCheckbox4');
+                                var subMainCheckbox4Options = document.getElementById('subMainCheckbox4Options');
+                            
+                                if (subMainCheckbox4Checkbox.checked) {
+                                    subMainCheckbox4Options.style.display = 'block';
+                                } else {
+                                    subMainCheckbox4Options.style.display = 'none';
+                                }
+                                updateSentence();
+                            }
+
+                            // SUB BOX 5-8 FUNCTION
+                            function togglesubMainCheckbox5Options() {
+                                var subMainCheckbox5Checkbox = document.getElementById('subMainCheckbox5');
+                                var subMainCheckbox5Options = document.getElementById('subMainCheckbox5Options');
+                            
+                                if (subMainCheckbox5Checkbox.checked) {
+                                    subMainCheckbox5Options.style.display = 'block';
+                                } else {
+                                    subMainCheckbox5Options.style.display = 'none';
                                 }
                                 updateSentence();
                             }
