e6dd1a7a63fed9d1970b5da8ddd2f5635808668b
Mohammed Salah <145399512+MSALAHAH@users.noreply.github.com>
2023-11-19 16:48:53 -0500

    Update screenAwake.bat

--- attach/screenAwake.bat
+++ attach/screenAwake.bat
@@ -1,1 +1,16 @@
-powercfg -change -standby-timeout-ac 720
+@echo off
+setlocal enabledelayedexpansion
+
+set "duration=720"
+
+set "interval=10"
+
+set /a "totalDuration=!duration! * !interval!"
+
+for /l %%i in (1, 1, %totalDuration%) do (
+    echo Simulating cursor movement...
+    nircmd.exe movecursor 1 1
+    timeout /nobreak %interval%
+)
+
+echo Done.
