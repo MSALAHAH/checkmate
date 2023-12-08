2273fee63dcd7f30cbf7b00cdfe7b2d34083ea24
Mohammed Salah <145399512+MSALAHAH@users.noreply.github.com>
2023-12-07 21:12:34 -0500

    chatUpdate

--- javaScript/ChatBotScript.js
+++ javaScript/ChatBotScript.js
@@ -1,27 +1,53 @@
 //              بِسْم اللَّه الرَّحْمن الرَّحِيم
 //  وَعَلَّمَكَ مَا لَمْ تَكُن تَعْلَمُ ۚ وَكَانَ فَضْلُ ٱللَّهِ عَلَيْكَ عَظِيمًۭا
-// const errorDatabase = [
-//     {
-//         errorMessage: "",
-//         solution: ``
-//     },
-//     {
-//         errorMessage: "",
-//         solution: ""
-//     },
-//     {
-//         errorMessage: "",
-//         solution: ""
-//     },
-//     {
-//         errorMessage: "",
-//         solution: ""
-//     },
-//     {
-//         errorMessage: "",
-//         solution: ""
-//     }
-// ];
+const errorDatabase = [
+    {
+        errorMessage: "acp",
+        solution:`
+        Get the Approval letter: getinternet.gov,
+        <br>
+        get the benefit with Optimum: optimum.com/acpapp,
+        <br>
+        for questions about eligibility or the verification process,
+        <br>
+        (888)891-2189,
+        <br>
+        <hr>
+        if he/she want need any help with something like the below,
+        <br>
+        1- The status of your ACP application,
+        <br>
+        2- Documents needed to show if he/she qualify,
+        <br>
+        3- Assistance with the ACP Household Worksheet,
+        <br>
+        4- Resetting the account.
+        <br>
+        call the ACP Support Center at (877)384-2575
+        <br>
+        (7 days a week, from 9:00 a.m. to 9:00 p.m. ET)
+        <br>
+        <hr>
+        or E-mail ACPSupport@usac.org.
+        `,
+    },
+    {
+        errorMessage: "",
+        solution: ""
+    },
+    {
+        errorMessage: "",
+        solution: ""
+    },
+    {
+        errorMessage: "",
+        solution: ""
+    },
+    {
+        errorMessage: "",
+        solution: ""
+    }
+];
 
 document.addEventListener("DOMContentLoaded", function () {
     const chatbot = document.querySelector(".chatbot");
@@ -91,33 +117,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const responses = {
             "hello": "Hello! How can I assist you today?",
             "hi": "Hi! How is it going today?",
-            "acp": `
-            Get the Approval letter: getinternet.gov,
-            <br>
-            get the benefit with Optimum: optimum.com/acpapp,
-            <br>
-            for questions about eligibility or the verification process,
-            <br>
-            (888)891-2189,
-            <br>
-            <hr>
-            if he/she want need any help with something like the below,
-            <br>
-            1- The status of your ACP application,
-            <br>
-            2- Documents needed to show if he/she qualify,
-            <br>
-            3- Assistance with the ACP Household Worksheet,
-            <br>
-            4- Resetting the account.
-            <br>
-            call the ACP Support Center at (877)384-2575
-            <br>
-            (7 days a week, from 9:00 a.m. to 9:00 p.m. ET)
-            <br>
-            <hr>
-            or E-mail ACPSupport@usac.org.
-            `,
             "how are you": "I'm just a computer program, but I'm here to help.",
             "goodbye": "Goodbye!",
             "default": "I'm not sure I understand. Can you please rephrase or ask another question?"
