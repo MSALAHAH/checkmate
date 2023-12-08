a667c6593e310bb9f37919b1a74a9b85f77a48cd
Mohammed Salah <145399512+MSALAHAH@users.noreply.github.com>
2023-12-07 19:19:28 -0500

    chatUpdate

--- javaScript/ChatBotScript.js
+++ javaScript/ChatBotScript.js
@@ -1,27 +1,27 @@
 //              بِسْم اللَّه الرَّحْمن الرَّحِيم
 //  وَعَلَّمَكَ مَا لَمْ تَكُن تَعْلَمُ ۚ وَكَانَ فَضْلُ ٱللَّهِ عَلَيْكَ عَظِيمًۭا
-const errorDatabase = [
-    {
-        errorMessage: "",
-        solution: ``
-    },
-    {
-        errorMessage: "",
-        solution: ""
-    },
-    {
-        errorMessage: "",
-        solution: ""
-    },
-    {
-        errorMessage: "",
-        solution: ""
-    },
-    {
-        errorMessage: "",
-        solution: ""
-    }
-];
+// const errorDatabase = [
+//     {
+//         errorMessage: "",
+//         solution: ``
+//     },
+//     {
+//         errorMessage: "",
+//         solution: ""
+//     },
+//     {
+//         errorMessage: "",
+//         solution: ""
+//     },
+//     {
+//         errorMessage: "",
+//         solution: ""
+//     },
+//     {
+//         errorMessage: "",
+//         solution: ""
+//     }
+// ];
 
 document.addEventListener("DOMContentLoaded", function () {
     const chatbot = document.querySelector(".chatbot");
@@ -94,8 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "how are you": "I'm just a computer program, but I'm here to help.",
             "goodbye": "Goodbye!",
             "default": "I'm not sure I understand. Can you please rephrase or ask another question?",
-            "acp": `
-            Get the Approval letter: getinternet.gov,
+            "acp": `Get the Approval letter: getinternet.gov,
             <br>
             get the benefit with Optimum: optimum.com/acpapp,
             <br>
